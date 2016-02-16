feae7aa2064b0be57a6e55bf63c97d9ac94748d6
Karl Johann Schubert <karljohann@familieschubi.de>
2016-02-17 00:17:39 +0100

    FIX curl output

--- CustomerIO.php
+++ CustomerIO.php
@@ -136,7 +136,7 @@ class CustomerIO {
         curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         curl_setopt($session, CURLOPT_HTTPGET, 1);
         curl_setopt($session, CURLOPT_HEADER, false);
-        curl_setopt($session, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($session, CURLOPT_VERBOSE, true);
         curl_setopt($session, CURLOPT_CUSTOMREQUEST, $requestType);
         curl_setopt($session, CURLOPT_POSTFIELDS, http_build_query($data));
